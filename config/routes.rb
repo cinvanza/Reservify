a6df0e71c19fe6ea8e1f65f9d364cbc5865d480c
Jose Cerrato <jfcerrato92@gmail.com>
2024-06-08 09:31:55 -0600

    Booking creation and show fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :flats do
-  resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:new, :create]
   end
+  resources :bookings, only: [:index, :destroy]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
