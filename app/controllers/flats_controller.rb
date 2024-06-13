f62ff0d445bed60f77f987caddb9b0ada2182a6b
vanesa camano <vanesasoledad90@gmail.com>
2024-06-13 23:39:15 +0100

    search filter

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -38,9 +38,39 @@ class FlatsController < ApplicationController
     redirect_to flats_path, status: :see_other
   end
 
+  def search
+
+    @flats = Flat.all
+    if params[:destination].present?
+      @flats = @flats.where(address: params[:destination])
+    end
+    @check_in = params[:available_start]
+    @check_out = params[:available_end]
+
+    if @check_in.present? && @check_out.present?
+      @available = available_dates(@check_in, @check_out)
+    else
+      @available = []
+      flash[:alert] = "Please enter both check-in and check-out dates."
+    end
+  end
+
   private
 
   def flat_params
     params.require(:flat).permit(:address, :price, :capacity, :user_id, :reserved, :title, :description, :image_url, :available_start, :available_end, photos: [])
+  end
+end
+
+def available_dates(check_in, check_out)
+  check_in_date = Date.parse(check_in)
+  check_out_date = Date.parse(check_out)
+
+  flats = Flat.where("check_in < ? AND check_out > ?", check_out_date, check_in_date)
+
+  if flats.exists?
+    return []
+  else
+    return [check_in_date, check_out_date]
   end
 end
